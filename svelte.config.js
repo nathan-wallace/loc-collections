306b8d457845f5d785eb8d539a310948080c870a
nathan-wallace <nathan.s.wallace@gmail.com>
2025-08-12 08:00:01 -0500

    fix: static site build and deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 // svelte.config.js
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    paths: {
+      base: process.env.BASE_PATH ?? ''
+    },
     alias: {
       $lib: 'src/lib',
       $components: 'src/components'
