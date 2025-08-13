9201c0b2014ca7d4aa9949be76aa8d7ed77b1a20
nathan-wallace <nathan.s.wallace@gmail.com>
2025-08-13 09:03:30 -0500

    feat: add book icon to site title

---
+++ src/components/BookIcon.svelte
@@ -0,0 +1,21 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-6 w-6">
+  <!-- Left cover (angled) -->
+  <path d="M6 14 C20 8, 28 12, 32 18 L32 50 C28 46, 20 42, 6 46 Z" fill="white"/>
+  
+  <!-- Right cover (angled) -->
+  <path d="M58 14 C44 8, 36 12, 32 18 L32 50 C36 46, 44 42, 58 46 Z" fill="white"/>
+  
+  <!-- Center spine with perspective -->
+  <path d="M32 18 L32 50" />
+  
+  <!-- Left page curves -->
+  <path d="M10 22 C20 20, 28 24, 28 24" />
+  <path d="M10 30 C20 28, 28 32, 28 32" />
+  
+  <!-- Right page curves -->
+  <path d="M36 24 C44 20, 54 22, 54 22" />
+  <path d="M36 32 C44 28, 54 30, 54 30" />
+  
+  <!-- Bottom edge for 3D effect -->
+  <path d="M6 46 L32 50 L58 46" />
+</svg>
